58495a19e7cc84f7288eb4c44005ae05cb8bfc64
adahyto <adahy.to@gmail.com>
2018-03-09 22:32:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+            function ShopController($scope) {
+                $scope.items = [
+                    {title: 'product_name', price: '0', quantity: '1'},
+                    {title: 'product_name', price: '0', quantity: '1'}
+                ];
+                $scope.totalCart = function() {
+                    var total = 0;
+                    for (var i = 0, len = $scope.items.length; i < len; i++) {
+                        total = total + $scope.items[i].price * $scope.items[i].quantity;
+                    }
+                    return total;
+                };
+                $scope.remove = function(index) {
+                    $scope.items.splice(index, 1);
+                };
+                $scope.add = function() {
+                    for(var i=0; i<1; i++) {
+                        $scope.items.push({title: 'product_name', price: '0', quantity: '1'});
+                    }
+                };
+            }
